11b2dcb7d327dee0847b26d6ee0da6a287825eb5
Sumeeth-24 <sumeethmoolyaa@gmail.com>
2026-08-23 15:15:46 +0530

    09: file explorer

--- src/app/projects/[projectId]/layout.tsx
+++ src/app/projects/[projectId]/layout.tsx
@@ -1,4 +1,5 @@
 import { ProjectIdLayout } from '@/features/projects/components/project-id-layout';
+import { Id } from '../../../../convex/_generated/dataModel';
 
 
 const layout = async ({
@@ -6,7 +7,7 @@ const layout = async ({
     params,
 }: {
   children: React.ReactNode;
-   params: Promise<{ projectId: string}>
+   params: Promise<{ projectId: Id<"projects">}>
 }) => {
     const {projectId} = await params;
 
