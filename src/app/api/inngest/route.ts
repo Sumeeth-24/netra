f8caad692c73a5fafa98db7606ad16b561e966a1
Sumeeth-24 <sumeethmoolyaa@gmail.com>
2026-09-12 14:33:18 +0530

    12: conversation system

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { processTask } from "@/inngest/functions";
+import { processMessage } from "@/features/conversations/inngest/process-message";
 
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [processTask],
+  functions: [processTask, processMessage],
 });
